cc52c64beb0a7b2c83295d68331fda5e95b3a7af
Ondřej Ševčík <35051006+SalvatoreDiAngelo@users.noreply.github.com>
2018-05-18 16:39:43 +0200

    Add files via upload

---
+++ InputAndOutputRandomAccess/src/com/sevcikon/IndexRecord.java
@@ -0,0 +1,30 @@
+package com.sevcikon;
+
+/**
+ * Created by ondrejsevcik on 09.05.2018.
+ **/
+public class IndexRecord {
+    private int startByte;
+    private int length;
+
+    public IndexRecord(int startByte, int length) {
+        this.startByte = startByte;
+        this.length = length;
+    }
+
+    public int getStartByte() {
+        return startByte;
+    }
+
+    public void setStartByte(int startByte) {
+        this.startByte = startByte;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
